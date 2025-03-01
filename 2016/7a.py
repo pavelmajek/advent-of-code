79c8efc0b1b8127c10ef22118e74e7573d5409b9
pavelmajek <p.majek@seznam.cz>
2025-03-01 03:20:38 +0100

    2016 day 1 to 10 added

---
+++ 2016/7a.py
@@ -0,0 +1,14 @@
+# Advent of Code 2016, day 7, part 1
+
+import re
+
+with open("data/7.txt") as file:
+    content = [line.strip() for line in file.readlines()]
+
+count = 0
+for line in content:
+    if not any(re.search(r"(?=(\w)(?!\1)(\w)\2\1)\w{4}", bracket_content) for bracket_content in re.findall(r"\[(.*?)\]", line)):
+        if re.search(r"(?=(\w)(?!\1)(\w)\2\1)\w{4}", line):
+            count += 1
+
+print(f"Count: {count}")
